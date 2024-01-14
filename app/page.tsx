90bc9b2892356a1abd14a63052eed6ea58a73e1b
DavTheDev20 <reiddavin@outlook.com>
2024-01-14 17:41:06 -0500

    Add feature where text will stay if user tries to submit while unauthenticated

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-'use client';
+"use client";
 
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import axios from 'axios';
-import Post from './components/Post';
-import Loading from './components/Loading';
-import { FormEvent, useState, useRef } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
-import { PostType } from '@/types/types';
-import { useSession } from 'next-auth/react';
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import Post from "./components/Post";
+import Loading from "./components/Loading";
+import { FormEvent, useState, useRef } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { PostType } from "@/types/types";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
   const maxCharLength = 250;
@@ -22,6 +22,15 @@ export default function Home() {
   const contentInput: { current: HTMLTextAreaElement } = useRef(null);
 
   async function getPosts() {
+    const form = document.querySelector("form");
+    if (localStorage.getItem("currTitle")?.length !== 0) {
+      // @ts-ignore
+      form.title.value = localStorage.getItem("currTitle");
+    }
+    if (localStorage.getItem("currContent")?.length !== 0) {
+      // @ts-ignore
+      form.content.value = localStorage.getItem("currContent");
+    }
     const response = await axios.get(`/api/posts`);
     const data = await response.data;
     let posts: Array<PostType> = data.posts;
@@ -30,7 +39,7 @@ export default function Home() {
   }
 
   const { isLoading, isError, data, error, isFetching } = useQuery({
-    queryKey: ['posts'],
+    queryKey: ["posts"],
     queryFn: getPosts,
   });
 
@@ -41,14 +50,16 @@ export default function Home() {
     onError: (error: any) => {
       if (error?.response.data.error)
         return toast.error(JSON.stringify(error?.response.data.error));
-      toast.error('Something went wrong');
+      toast.error("Something went wrong");
     },
     onSuccess: () => {
-      console.log('Working..');
-      titleInput.current.value = '';
-      contentInput.current.value = '';
+      console.log("Working..");
+      titleInput.current.value = "";
+      contentInput.current.value = "";
+      localStorage.removeItem("currTitle");
+      localStorage.removeItem("currContent");
       setContentCharCount(0);
-      queryClient.invalidateQueries({ queryKey: ['posts'] });
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
     },
   });
 
@@ -57,17 +68,20 @@ export default function Home() {
 
     // Error if user is not signed in
     if (!session.data) {
-      titleInput.current.value = '';
-      contentInput.current.value = '';
-      return toast.error('Please login to submit post');
+      console.log(event.target);
+      //@ts-ignore
+      localStorage.setItem("currTitle", event.target.title.value as string);
+      //@ts-ignore
+      localStorage.setItem("currContent", event.target.content.value as string);
+      return toast.error("Please login to submit post");
     }
 
     const formData = new FormData(event.target as HTMLFormElement);
-    const title = formData.get('title') as string;
-    const content = formData.get('content') as string;
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
 
     if (!title || !content) {
-      toast.error('Please include both a title and content');
+      toast.error("Please include both a title and some content");
       return;
     }
 
@@ -96,20 +110,33 @@ export default function Home() {
             placeholder="What's the title?"
             autoComplete="off"
             ref={titleInput}
+            onChange={(event) => {
+              if (
+                localStorage.getItem("currTitle") &&
+                event.target.value.length === 0
+              ) {
+                localStorage.removeItem("currTitle");
+              }
+            }}
           />
           <textarea
             name="content"
             className="block my-3 w-full h-28 px-1 py-2 rounded-md text-lg"
             placeholder="say whats on your mind... 🤔"
-            onChange={(event) => setContentCharCount(event.target.value.length)}
+            onChange={(event) => {
+              setContentCharCount(event.target.value.length);
+              if (event.target.value.length === 0) {
+                localStorage.removeItem("currContent");
+              }
+            }}
             ref={contentInput}
           />
           <label
             className="block"
             style={
               contentCharCount > maxCharLength
-                ? { color: 'red' }
-                : { color: '#6b7280' }
+                ? { color: "red" }
+                : { color: "#6b7280" }
             }
           >
             {contentCharCount}/{maxCharLength}
@@ -125,7 +152,7 @@ export default function Home() {
         {isLoading || isFetching ? (
           <Loading />
         ) : isError ? (
-          'Error...'
+          "Error..."
         ) : (
           data.map((post: PostType) => {
             return (
